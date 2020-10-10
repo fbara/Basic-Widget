f69255aecf29e97d5c8f9a15ffd9c68fceee5152
fbara <fbara1@yahoo.com>
2020-10-09 21:39:49 -0500

    Initial Commit

---
+++ Basic Widget/Basic_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_WidgetApp.swift
+//  Basic Widget
+//
+//  Created by Frank Bara on 10/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Basic_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Basic Widget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Basic Widget
+//
+//  Created by Frank Bara on 10/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
